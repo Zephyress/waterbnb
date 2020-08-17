1838caa1885c730154c74ebd6c8668d0e8a3cd71
Mathilda Djamdjian <mathilda.djamdjian@gmail.com>
2020-08-17 16:54:32 +0200

    V1 update_feature

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,11 @@
 class BoatsController < ApplicationController
+  def edit
+    @boat = Boat.find(params)
+  end
+
+  def update
+    @boat = Boat.find(params[:id])
+    @boat.update(task_params)
+    redirect_to boats_path
+  end
 end
