ddd12a71f2e8d1ac2c12b8720d90b6d20620ead2
Nancaidah Toure Chauvin <nancaidahct@gmail.com>
2020-08-17 17:10:36 +0200

    index created

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,11 @@
 class BoatsController < ApplicationController
+  def index
+    @boats = Boat.all
+  end
+
+  def destroy
+    @boat = Boat.find(params[:id])
+    @boat.destroy
+    redirect_to boats_path
+  end
 end
