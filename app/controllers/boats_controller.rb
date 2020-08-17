4f34e8755fc74f8e856974a1a3b31394762a72d7
Mathilda Djamdjian <mathilda.djamdjian@gmail.com>
2020-08-17 17:38:10 +0200

    V2 update_feature

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,11 +1,21 @@
 class BoatsController < ApplicationController
+  before_action :find_boat, only:[:edit, :update]
+
   def edit
-    @boat = Boat.find(params)
   end
 
   def update
-    @boat = Boat.find(params[:id])
-    @boat.update(task_params)
+    @boat.update(boat_params)
     redirect_to boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :description, :category, :price)
+  end
+
+  def find_boat
+    @Boat = Boat.find(params[:id])
   end
 end
