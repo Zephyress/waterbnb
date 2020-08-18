f91786734eea2c1091721ea43f9b0b4b613891f1
Gregoire Pothier <pothier.gregoire@gmail.com>
2020-08-18 16:01:18 +0200

    Seeded wtih user and boat pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,35 +3,40 @@ require 'open-uri'
 require 'nokogiri'
 CATEGORIES = ["Gulet", "Motoryatch", "Motor Boat", "Power catamaran", "Jet-ski", "Catamaran", "Sailing yatch"]
 
-puts " \u{26f5} Sinking the boats \u{26f5} "
+puts " \u{26f5} Sinking the existing boats \u{26f5} "
 puts
 Boat.destroy_all
-puts " \u{1f52a} Killing the users \u{1f52a} "
+puts " \u{1f52a} Killing the registered users \u{1f52a} "
 User.destroy_all
 puts
 
 puts " ------- \u{2728} !DB Clean! \u{2728} ------- "
 puts
+
+
+
+
+# html_file = open(
+#   url,
+#   'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
+#   'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+#   ).read
+# html_doc = Nokogiri::HTML(html_file
+
 puts "\u{1f476} Giving birth to new users \u{1f476}"
 20.times do
-  User.create!(
+  user = User.create!(
     name:Faker::FunnyName.name,
     email:Faker::Internet.email,
     password:"pouetpouet")
+  file = URI.open('https://www.fillmurray.com/640/360')
+user.photo.attach(io: file, filename: 'Bill.jpg', content_type: 'image/jpg')
   end
 puts
-puts "\u{1f46f} CONGRATS! It's #{User.count} users \u{1f46f}"
+puts "\u{1f46f} CONGRATS! It's #{User.count} users, half of them (give or take) are girls \u{1f46f}"
 puts
 puts " \u{1f6e0}  Building new boats \u{1f6e0} "
 
-url = "https://www.yachtworld.com/boats-for-sale/type-sail/"
-
-html_file = open(
-  url,
-  'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
-  'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-  ).read
-html_doc = Nokogiri::HTML(html_file)
 images = ['./app/assets/images/boat1.png','./app/assets/images/boat2.png','./app/assets/images/boat3.png','./app/assets/images/boat4.png','./app/assets/images/boat5.png','./app/assets/images/boat6.png','./app/assets/images/boat7.png','./app/assets/images/boat8.png','./app/assets/images/boat9.png']
 # html_doc.search('img').each do |element|
 #   # images << element.attribute('src').value
@@ -47,10 +52,10 @@ images = ['./app/assets/images/boat1.png','./app/assets/images/boat2.png','./app
     description: Faker::GreekPhilosophers.quote,
     user_id: User.all.sample.id
   )
-  file.open(images.sample)
+  file = File.open(images.sample)
   boat.photo.attach(io: file, filename: "${boat.title}.jpg", content_type: 'image/jpg')
 end
-
+puts
 puts "\u{2693} #{Boat.count} boats in the harbor \u{2693}"
 
 # This file should contain all the record creation needed to seed the database with its default values.
