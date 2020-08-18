e8951fb62736609440c3fa9140518c0ad7e4983f
romain mirabile <romainmirabile@hotmail.fr>
2020-08-18 12:48:25 +0200

    First version of the bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @boat = Boat.find(params[:boat_id])
+    @booking = Booking.new
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @booking.boat = @boat
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
