0ac89ee14fa84b65ce711d24046a714f8facb885
romain mirabile <romainmirabile@hotmail.fr>
2020-08-19 11:51:48 +0200

    Latitude et Longitude rajouté dans le document

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_091333) do
+ActiveRecord::Schema.define(version: 2020_08_19_093343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_08_19_091333) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
