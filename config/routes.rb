1f99b46386fe3de99d615abdf3cae2840500e404
Nancaidah Toure Chauvin <nancaidahct@gmail.com>
2020-08-20 13:03:27 +0200

    route and page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get 'dashboard', to: 'bookings#index'
+  end
+
   root to: 'boats#index'
   resources :boats do
     resources :bookings, only: [:create, :new]
   end
+
+  resources :bookings, only: [:index, :show]
+
   # POST 'pages/login', to: 'pages#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
